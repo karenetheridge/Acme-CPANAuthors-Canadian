6fd34f652458f4122a19411618f0fcc4137ae635
Karen Etheridge <ether@cpan.org>
2016-02-01 20:14:17 -0800

    convert to my plugin bundle

--- lib/Acme/CPANAuthors/Canadian.pm
+++ lib/Acme/CPANAuthors/Canadian.pm
@@ -1,9 +1,10 @@
 package Acme::CPANAuthors::Canadian;
+# ABSTRACT: We are Canadian CPAN authors
 
 use warnings;
 use strict;
 
-our $VERSION = '0.0106';
+our $VERSION = '0.0107';
 
 use Acme::CPANAuthors::Register (
     ZOFFIX => 'Zoffix Znet',
@@ -21,12 +22,7 @@ use Acme::CPANAuthors::Register (
 1;
 __END__
 
-=encoding utf8
-
-=head1 NAME
-
-Acme::CPANAuthors::Canadian - We are Canadian CPAN authors
-
+=pod
 =head1 SYNOPSIS
 
     use strict;
@@ -67,52 +63,4 @@ We are Canadian CPAN authors:
 If you are a Canadian CPAN author not listed here, please send me your ID/name
 via email or RT so we can always keep this module up to date.
 
-=head1 AUTHOR
-
-'Zoffix, C<< <'zoffix at cpan.org'> >>
-(L<http://zoffix.com/>, L<http://haslayout.net/>, L<http://zofdesign.com/>)
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-acme-cpanauthors-canadian at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-CPANAuthors-Canadian>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::CPANAuthors::Canadian
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-CPANAuthors-Canadian>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Acme-CPANAuthors-Canadian>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Acme-CPANAuthors-Canadian>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Acme-CPANAuthors-Canadian>
-
-=back
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 'Zoffix, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
 =cut
-
